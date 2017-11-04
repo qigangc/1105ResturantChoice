2d2a98c23b90396d9f445163fd47b9324a65430f
Edwy(Qigang) Cai <qigangc@email.arizona.edu>
2017-11-04 12:43:50 -0700

    Add files via upload

--- sample3.0/src/application/Main.java
+++ sample3.0/src/application/Main.java
@@ -1,5 +1,14 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,23 +20,47 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Read file fxml and draw interface.
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("/application/MyScene.fxml"));
+	public static List resChinese;
+	public static List resWithOutChinese;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		initializeApplication();
+		try {
+			// Read file fxml and draw interface.
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MyScene.fxml"));
 
-            primaryStage.setTitle("1105饭店选择器");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
+			primaryStage.setTitle("1105饭店选择器");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
 
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	private void initializeApplication() {
+		String line = null;
+		resChinese = new ArrayList();
+		resWithOutChinese = new ArrayList();
+		try {
+			FileReader fileReader = new FileReader("Tucson_Resturent_chinese");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while((line = bufferedReader.readLine()) != null) 
+               		resChinese.add(line.trim());
+			fileReader = new FileReader("Tucson_Resturent_noChinese");
+			bufferedReader = new BufferedReader(fileReader);
+			while((line = bufferedReader.readLine()) != null)
+					resWithOutChinese.add(line.trim());
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable Find the file.");
+		} catch (IOException e) {
+			System.out.println("Unable Find the file.");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- sample3.0/src/application/MyController.java
+++ sample3.0/src/application/MyController.java
@@ -18,7 +18,6 @@ public class MyController implements Initializable {
 
    @FXML
    private Button myButton;
-
    @FXML
    private TextField myTextField;
    @FXML
@@ -27,25 +26,16 @@ public class MyController implements Initializable {
    private ChoiceBox myChoiceBox_price;
    @FXML
    private ChoiceBox myChoiceBox_distance;
-
-   private String[] res = new String[20];
    
    @Override
    public void initialize(URL location, ResourceBundle resources) {
-	   res[0] = "Brother Noodle";
-	   res[1] = "KongFu Noodle";
-	   res[2] = "MeiMei";
-	   res[3] = "PoP Eye";
-	   res[4] = "Apple Bee";
 	   myChoiceBox.getItems().add("Chinese Food");
 	   myChoiceBox.getItems().add("W/O Chinese Food");
-	   myChoiceBox_price.getItems().add("5-15");
-	   myChoiceBox_price.getItems().add("15-25");
+	   myChoiceBox_price.getItems().add("5-15$");
+	   myChoiceBox_price.getItems().add("15-25$");
 	   myChoiceBox_distance.getItems().add("0-5miles");
 	   myChoiceBox_distance.getItems().add("5-10miles");
 	   myChoiceBox_distance.getItems().add(">10miles");
-       // TODO (don't really need to do anything here).
-
    }
 
    // When user click on myButton
@@ -57,14 +47,22 @@ public class MyController implements Initializable {
       if (resType == null){
           myTextField.setText("No Type");
           return;}
-      else if (resType == "Chinese Food")
-    	  i = (int)(Math.random() * ((2 - 0) + 1) + 0);
-      else if (resType == "W/O Chinese Food")
-    	  i = (int)(Math.random() * ((4 - 3) + 1) + 3);
-      //System.out.println("Button Clicked!");
-      //System.out.println("num=" + i);
-      toPrint = res[i];
-      myTextField.setText(toPrint);
+      else if (resType == "Chinese Food"){
+    	  i = (int)(Math.random() * Main.resChinese.size());
+    	  toPrint = (String) Main.resChinese.get(i);
+    	  myTextField.setText(toPrint);
+    	  return;
+      }
+      else if (resType == "W/O Chinese Food"){
+    	  i = (int)(Math.random() * Main.resWithOutChinese.size());
+    	  toPrint = (String) Main.resWithOutChinese.get(i);
+    	  myTextField.setText(toPrint);
+    	  return;
+      }
+      System.out.println("Button Clicked!");
+      System.out.println("num=" + i);
+      //toPrint = ;
+      //myTextField.setText(toPrint);
 
    }
 
